3c9d68a57b9dc1183a44c191b23fc729cd6a0423
gushouchuanzhi1 <116015021+gushouchuanzhi1@users.noreply.github.com>
2023-12-06 21:58:42 +0800

    20231206学习部分
    20231206学习部分

---
+++ study/src/day1/content1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class content1 {
+    public static void main(String[] args) {
+        /**
+         * 无限循环多种表示
+         * */
+        while (true){
+            System.out.println("true");
+        }
+        /*if(){
+            System.out.println("true");
+        }*/
+    }
+}
